97f6a8ad25207e086677584e961eac8770375366
Fitsum Abebe <faabebe@fabric.com>
2020-04-01 22:34:47 -0400

    new:template for document db

--- terraform/document_db/cluster-instance.tf
+++ terraform/document_db/cluster-instance.tf
@@ -3,7 +3,6 @@ variable "cluster_id" {}
 variable "instance_prefix" {}
 variable "instance_type" {}
 variable "instance_count" {}
-variable "secrets" {}
 
 locals {
   env = terraform.workspace
@@ -13,11 +12,15 @@ locals {
     "${var.region}c"]
 }
 
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "document-db/master_creds"
+}
+
 resource "aws_docdb_cluster" "default" {
   cluster_identifier = "${var.cluster_id}"
   availability_zones = local.azs
-  master_username    = "test"
-  master_password    = "testasdfj2342"
+  master_username    = jsondecode("${data.aws_secretsmanager_secret_version.creds.secret_string}")["username"]
+  master_password    = jsondecode("${data.aws_secretsmanager_secret_version.creds.secret_string}")["password"]
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
@@ -26,5 +29,3 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   cluster_identifier = "${aws_docdb_cluster.default.id}"
   instance_class     = "${var.instance_type}"
 }
-
-
