eb4e3a2e4e8a98c158a626d447e2cfe046ac5e83
Nati A <natiabebe12@gmail.com>
2020-04-01 22:35:45 -0700

    created param_group to disable tls

--- terraform/document_db/cluster-instance.tf
+++ terraform/document_db/cluster-instance.tf
@@ -16,11 +16,25 @@ data "aws_secretsmanager_secret_version" "creds" {
   secret_id = "document-db/master_creds"
 }
 
+resource "aws_docdb_cluster_parameter_group" "tls-disabled" {
+  family      = "docdb3.6"
+  name        = "tls-disabled"
+  description = "docdb cluster parameter group"
+
+  parameter {
+    name  = "tls"
+    value = "disabled"
+  }
+}
+
+
 resource "aws_docdb_cluster" "default" {
   cluster_identifier = "${var.cluster_id}"
   availability_zones = local.azs
   master_username    = jsondecode("${data.aws_secretsmanager_secret_version.creds.secret_string}")["username"]
   master_password    = jsondecode("${data.aws_secretsmanager_secret_version.creds.secret_string}")["password"]
+  backup_retention_period = 5
+  db_cluster_parameter_group_name = "${aws_docdb_cluster_parameter_group.tls-disabled.name}"
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
